636169b5ecf3a61126f333417589fcdf08dd9be6
dangeng <dangengdg@gmail.com>
2023-12-22 16:09:18 -0500

    Viz is pretty much working (testing mobile)

--- static/js/corr.js
+++ static/js/corr.js
@@ -1,3 +1,85 @@
+////////////////////////
+/// HELPER FUNCTIONS ///
+////////////////////////
+
+/*
+Function to load arrows (flow) from json
+Should be of format:
+    [
+        [dx, dy], [dx, dy], [dx, dy] ...
+        [dx, dy], [dx, dy], [dx, dy] ...
+        [dx, dy], [dx, dy], [dx, dy] ...
+        ...
+    ]
+    such that data[x][y] gives offsets in x and y dirs
+*/
+async function loadFlow(path) {
+    try {
+        console.log('Loading json from: ' + path)
+        const response = await fetch(path);
+        const flow = await response.json();
+        return flow;
+    } catch (error) {
+        console.error("Error loading flow:", error);
+        return [];
+    }
+}
+
+
+
+/////////////////////////
+/// DRAWING FUNCTIONS ///
+/////////////////////////
+
+// Draws an arrow on the canvas
+// From: https://codepen.io/chanthy/pen/WxQoVG
+function drawArrow(canvas, fromx, fromy, tox, toy, arrowWidth, color){
+    // Get canvas context
+    ctx = canvas.getContext("2d");
+
+    //variables to be used when creating the arrow
+    var headlen = 10;
+    var angle = Math.atan2(toy-fromy,tox-fromx);
+ 
+    ctx.save();
+    ctx.strokeStyle = color;
+ 
+    //starting path of the arrow from the start square to the end square
+    //and drawing the stroke
+    ctx.beginPath();
+    ctx.moveTo(fromx, fromy);
+    ctx.lineTo(tox, toy);
+    ctx.lineWidth = arrowWidth;
+    ctx.stroke();
+ 
+    //starting a new path from the head of the arrow to one of the sides of
+    //the point
+    ctx.beginPath();
+    ctx.moveTo(tox, toy);
+    ctx.lineTo(tox-headlen*Math.cos(angle-Math.PI/7),
+               toy-headlen*Math.sin(angle-Math.PI/7));
+ 
+    //path from the side point of the arrow, to the other side point
+    ctx.lineTo(tox-headlen*Math.cos(angle+Math.PI/7),
+               toy-headlen*Math.sin(angle+Math.PI/7));
+ 
+    //path from the side point back to the tip of the arrow, and then
+    //again to the opposite side point
+    ctx.lineTo(tox, toy);
+    ctx.lineTo(tox-headlen*Math.cos(angle-Math.PI/7),
+               toy-headlen*Math.sin(angle-Math.PI/7));
+ 
+    //draws the paths created above
+    ctx.stroke();
+    ctx.restore();
+}
+
+// Clears the canvas
+function clearCanvas(canvas) {
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+}
+
+// Draws a circle on a canvas
 function drawCircle(canvas, radius, color, x, y) {
     var ctx = canvas.getContext('2d');
 
@@ -9,112 +91,167 @@ function drawCircle(canvas, radius, color, x, y) {
 }
 
 /*
-Given an array of displacements (see `loadArrowDirectionsFromDisk`)
-and an event, draw an arrow on the given canvas
+Given:
+    - an event (indicating which canvas is hovered over)
+    - a triplet of canvases, representing a visualization example
+    - an array of displacements (see `loadFlow`)
+
+Draw:
+    - an arrow on the flow
+    - an arrow on the src image
+    - circles representing corresponding points on the src and gen images
 */
-function updateCircle(event, canvas_tgt, directions) {
-    // Get canvas
-    var thisCanvas = event.target;
+function draw(event, canvases, flow) {
+    // Get event canvas (either 'src' or 'flow' canvas)
+    var eventCanvas = event.target;
 
-    // Get current size of the canvas
-    const canvasWidth = thisCanvas.width;
-    const canvasHeight = thisCanvas.height;
+    // Get current size of the canvas (just use 'src' canvas)
+    const canvasWidth = canvases['src'].width;
+    const canvasHeight = canvases['src'].height;
 
-    // Get mouse location, relative to canvas
-    var rect = thisCanvas.getBoundingClientRect();
-    const mouseX = Math.round((event.clientX - rect.left) * (canvasWidth / rect.width));
-    const mouseY = Math.round((event.clientY - rect.top) * (canvasHeight / rect.height));
+    // Get mouse/touch location
+    // Check if it's a touch event
+    if (event.touches && event.touches.length > 0) {
+        x = event.touches[0].clientX;
+        y = event.touches[0].clientY;
+    } else {
+        // It's a mouse event
+        x = event.clientX;
+        y = event.clientY;
+    }
+
+    // Get mouse location, relative to event canvas
+    var rect = eventCanvas.getBoundingClientRect();
+    const mouseX = Math.round(
+                (x - rect.left) * (canvasWidth / rect.width)
+            );
+    const mouseY = Math.round(
+                (y - rect.top) * (canvasHeight / rect.height)
+            );
 
     // Get size of arrow
-    const arrowSize = directions[mouseY][mouseX];
+    const arrowSize = flow[mouseY][mouseX];
 
-    // Draw circle
-    clearCanvas(thisCanvas);
-    drawCircle(thisCanvas, 15, 'blue', mouseX, mouseY);
+    // Get end point of arrow
+    const arrowX = mouseX + arrowSize[0];
+    const arrowY = mouseY + arrowSize[1];
 
-    // Draw corresponding circle on other canvas
-    clearCanvas(canvas_tgt);
-    drawCircle(canvas_tgt, 15, 'blue', mouseX + arrowSize[0], mouseY + arrowSize[1]);
+    // Clear canvases
+    clearCanvas(canvases['flow']);
+    clearCanvas(canvases['src']);
+    clearCanvas(canvases['gen']);
 
-    // Also draw arrow on source canvas
-    // If arrow is size 0, don't draw it
-    if (arrowSize[0] === 0 && arrowSize[1] === 0) {
-    } else {
-        // Draw a new arrow
-        const arrowX = mouseX + arrowSize[0];
-        const arrowY = mouseY + arrowSize[1];
+    // Draw circle on 'src' canvas
+    drawCircle(canvases['src'], 15, 'blue', mouseX, mouseY);
+
+    // Draw corresponding circle on 'gen' canvas
+    drawCircle(canvases['gen'], 15, 'blue', arrowX, arrowY);
+
+    // Draw arrows on 'src' and 'flow' canvases
+    // only if arrow is non-zero
+    if (arrowSize[0] != 0 || arrowSize[1] != 0) {
         drawArrow(
-            thisCanvas.getContext("2d"), 
+            canvases['src'], 
             mouseX, mouseY, 
             arrowX, arrowY, 
             8, 'red'
         );
+        drawArrow(
+            canvases['flow'], 
+            mouseX, mouseY, 
+            arrowX, arrowY, 
+            8, 'black'
+        );
     }
 }
 
+
+
+///////////////////////
+/// SETUP FUNCTIONS ///
+///////////////////////
+
 /*
-Sets up mouseleave and mousemove listeners for 
-a given canvas and a given array of displacements
+Sets up mouseleave and mousemove listeners for an example (a
+triplet of canvases) and a given array of displacements (flow)
 */
-function setupEventListenersPair(canvas_src, canvas_tgt, directions) {
-    // Curry the updateCircle function for mousemove event
-    function updateCircleWithDirections(event) {
-        updateCircle(event, canvas_tgt, directions);
+function setupEventListeners(example, flow) {
+    // Bind `example` and `direction` to 
+    // draw function for mousemove event
+    function drawPartial(event) {
+        draw(event, example, flow);
     }
 
-    // Clear the canvas on mouseleave event
+    // Clear all canvases on mouseleave event
     function handleMouseLeave(event) {
-        // Get canvas
-        var thisCanvas = event.target;
-        clearCanvas(thisCanvas);
-        clearCanvas(canvas_tgt);
+        clearCanvas(example['flow']);
+        clearCanvas(example['src']);
+        clearCanvas(example['gen']);
     }
 
     // Add listeners
-    canvas_src.addEventListener("mouseleave", handleMouseLeave);
-    canvas_src.addEventListener("mousemove", updateCircleWithDirections);
+    example['src'].addEventListener("mouseleave", handleMouseLeave);
+    example['src'].addEventListener("mousemove", drawPartial);
+    example['flow'].addEventListener("mouseleave", handleMouseLeave);
+    example['flow'].addEventListener("mousemove", drawPartial);
+
+    // Add listeners for touch (mobile)
+    example['src'].addEventListener('touchmove', function (e) {
+        // Prevent default to avoid unwanted behavior like scrolling
+        e.preventDefault();
+        drawPartial(e);
+    });
+    example['flow'].addEventListener('touchmove', function (e) {
+        // Prevent default to avoid unwanted behavior like scrolling
+        e.preventDefault();
+        drawPartial(e);
+    });
+    //example['src'].addEventListener('touchend', handleMouseLeave);
 }
 
 
 /*
-Initializes a given canvas:
+Initializes a given example (triplet of canvases):
     1. Loads the json displacement data
-    2. Waits for loading to finish, then adds event listeners to canvas
+    2. Waits for loading to finish, then adds event listeners to canvases
 */
-function initializeCanvasPair(canvasPair) {
+function initializeExample(example) {
     console.log('initializing canvas pair');
-    loadArrowDirectionsFromDisk(canvasPair['src'].dataset.jsonPath).then(directions => {
-        setupEventListenersPair(canvasPair['src'], canvasPair['gen'], directions);
+    // TODO: Remove all other data-json-path attributes
+    const jsonPath = example['flow'].dataset.jsonPath;
+    loadFlow(jsonPath).then(flow => {
+        setupEventListeners(example, flow);
     });
 }
 
-// Initialize all canvases
-function initializeCanvasPairs() {
-    var canvasPairs = {};
+// Initialize all examples (triplets of canvases)
+function initializeExamples() {
+    var examples = {};
 
-    var canvases = document.querySelectorAll('canvas.corrViz');
+    var canvases = document.querySelectorAll('canvas.viz');
 
-    // Organize canvases into pairs based on their common class
+    // Organize canvases into examples based on their common class
     canvases.forEach(function (canvas) {
         var classes = canvas.classList;
-        var pairClass = classes[1]; // example name
-        var individualClass = classes[2]; // gen or src
+        var name = classes[1]; // name of example
+        var vizType = classes[2]; // gen or src or flow
 
         // Initialize the index if it doesn't exist
-        if (!canvasPairs[pairClass]) {
-            canvasPairs[pairClass] = {};
+        if (!examples[name]) {
+            examples[name] = {};
         }
 
-        // Add the canvas to the index based on both pair and individual classes
-        canvasPairs[pairClass][individualClass] = canvas;
+        // Add the canvas to the index based on both name and vizType
+        examples[name][vizType] = canvas;
     });
 
-    for (var pairClass in canvasPairs) {
-        var canvasPair = canvasPairs[pairClass];
-        initializeCanvasPair(canvasPair);
+    // Initialize all examples
+    for (var name in examples) {
+        var example = examples[name];
+        initializeExample(example);
     };
 }
 
-// Initialize all canvases on DOM loaded
-document.addEventListener("DOMContentLoaded", initializeCanvasPairs);
+// Initialize all examples on DOM loaded
+document.addEventListener("DOMContentLoaded", initializeExamples);
 
